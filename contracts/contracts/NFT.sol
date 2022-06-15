20d8c9e5404bab8ebdc8af2bcccb188bfb996d28
dplamenov <dimitar.plamenov@gmail.com>
2022-06-15 16:51:49 +0300

    Create NFT.sol

---
+++ contracts/contracts/NFT.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract NFT is ERC721{
+    constructor(uint256 initialSupply) ERC20("NFT", "NFT") { }
+}

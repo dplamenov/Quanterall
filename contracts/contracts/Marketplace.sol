486d2d003bfa0764a3255a8f3bdb30aaca3db3e4
dplamenov <dimitar.plamenov@gmail.com>
2022-06-16 10:04:17 +0300

    update license

--- contracts/contracts/Marketplace.sol
+++ contracts/contracts/Marketplace.sol
@@ -1,9 +1,8 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
-
 
 contract Marketplace is ReentrancyGuard {
     address payable public immutable feeAccount;

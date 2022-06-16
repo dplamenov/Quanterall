6f9e295b87f5e168552bb5e91b459e15c5a30aba
dplamenov <dimitar.plamenov@gmail.com>
2022-06-16 14:39:09 +0300

    Update NFTToken.js

--- frontend/src/pages/NFTToken.js
+++ frontend/src/pages/NFTToken.js
@@ -1,8 +1,20 @@
-import {Typography} from "@mui/material";
+import {Typography, Container, TextField} from "@mui/material";
 
 function NFTToken() {
   return <>
     <Typography component='h1' variant='h1'>NFT Token</Typography>
+    <Container disableGutters maxWidth={false} sx={{display: 'flex'}}>
+      <Container disableGutters maxWidth={false}>
+        <Typography component='h2' variant='h2'>Buy</Typography>
+        <TextField id="buy-tokens-input" label="Tokens" variant="outlined" />
+        <TextField id="buy-eth-input" label="Eth" variant="outlined"/>
+      </Container>
+      <Container disableGutters maxWidth={false}>
+        <Typography component='h2' variant='h2'>Sell</Typography>
+        <TextField id="sell-eth-input" label="Eth" variant="outlined" />
+        <TextField id="sell-tokens-input" label="Tokens" variant="outlined"/>
+      </Container>
+    </Container>
   </>
 }
 

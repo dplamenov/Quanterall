d06a4df2a8d6054a47f778917fe9d8094f3979a5
dplamenov <dimitar.plamenov@gmail.com>
2022-06-16 10:11:42 +0300

    show ether balance in profile page

--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -1,12 +1,23 @@
 import {Typography} from "@mui/material";
 import {useSelector} from "react-redux";
+import {useEffect, useState} from "react";
+import {ethers} from 'ethers';
 
 function Profile() {
   const data = useSelector(state => state.web3);
+  const [balance, setBalance] = useState(0);
+  
+  useEffect(() => {
+    data.provider.getBalance(data.account).then((balance) => {
+      const balanceInEth = ethers.utils.formatEther(balance)
+      setBalance(balanceInEth);
+    })
+  });
 
   return <>
     <Typography variant='h1' component='h1'>Profile</Typography>
     <Typography component='p'>Address: {data.account}</Typography>
+    <Typography component='p'>Ether balance: {balance} ETH</Typography>
   </>
 }
 

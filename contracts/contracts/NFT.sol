0c226f0dff5029a04099ddfb12079abff6a2c125
dplamenov <dimitar.plamenov@gmail.com>
2022-06-16 10:00:19 +0300

    update nft contract

--- contracts/contracts/NFT.sol
+++ contracts/contracts/NFT.sol
@@ -1,8 +1,17 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
-contract NFT is ERC721{
-    constructor() ERC20("NFT", "NFT") { }
+contract NFT is ERC721URIStorage{
+    uint256 public tokenCount;
+
+    constructor() ERC721("NFT", "NFT") { }
+
+    function mint(string memory _tokenURI) external returns (uint256) {
+        tokenCount++;
+        _safeMint(msg.sender, tokenCount);
+        _setTokenURI(tokenCount, _tokenURI);
+        return (tokenCount);
+    }
 }

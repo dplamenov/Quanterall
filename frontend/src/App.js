b7fac528a06f3655a1ea450d056445ec071accb2
dplamenov <dimitar.plamenov@gmail.com>
2022-06-16 09:05:31 +0300

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,25 @@
 import Header from "./components/Header";
-import {BrowserRouter} from 'react-router-dom';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import {Provider} from 'react-redux';
 import {store} from './stores/store';
+import Profile from "./pages/Profile";
+import HomePage from "./pages/HomePage";
+import {Container} from "@mui/material";
 
 function App() {
   return (
-      <Provider store={store}>
-        <BrowserRouter>
-          <Header />
-        </BrowserRouter>
-      </Provider>
-    );
+    <Provider store={store}>
+      <BrowserRouter>
+        <Header />
+        <Container>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/profile" element={<Profile />} />
+          </Routes>
+        </Container>
+      </BrowserRouter>
+    </Provider>
+  );
 }
 
 export default App;

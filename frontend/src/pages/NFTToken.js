caa13770e17e04cf656ab5eb518a381a2e49a100
dplamenov <dimitar.plamenov@gmail.com>
2022-06-16 14:34:57 +0300

    create nft token page

---
+++ frontend/src/pages/NFTToken.js
@@ -0,0 +1,9 @@
+import {Typography} from "@mui/material";
+
+function NFTToken() {
+  return <>
+    <Typography component='h1' variant='h1'>NFT Token</Typography>
+  </>
+}
+
+export default NFTToken;

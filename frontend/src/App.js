36b0ae5ca1bb95dd4a97d409a27b19496cd847a9
dplamenov <dimitar.plamenov@gmail.com>
2022-06-16 14:35:13 +0300

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,7 @@ import {Container} from "@mui/material";
 import PrivateRoute from "./PrivateRoute";
 import Create from "./pages/Create";
 import Marketplace from "./pages/Marketplace";
+import NFTToken from "./pages/NFTToken";
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
             <Route path='/profile' element={<PrivateRoute element={<Profile />}/>} />
             <Route path='/create' element={<PrivateRoute element={<Create />}/>} />
             <Route path='/marketplace' element={<PrivateRoute element={<Marketplace />}/>} />
+            <Route path='/nft-token' element={<PrivateRoute element={<NFTToken />}/>} />
           </Routes>
         </Container>
       </BrowserRouter>

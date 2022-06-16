d6ef2d16fe769d171b6a7c71a260470f40d67c2c
dplamenov <dimitar.plamenov@gmail.com>
2022-06-16 12:23:57 +0300

    Update Create.js

--- frontend/src/pages/Create.js
+++ frontend/src/pages/Create.js
@@ -1,7 +1,59 @@
-import {Typography} from "@mui/material";
+import { useState } from 'react';
+import { create as ipfsHttpClient } from 'ipfs-http-client'
+import { ethers } from 'ethers';
+import { useNavigate } from "react-router-dom";
+import {useSelector} from "react-redux";
+import contracts from "../contracts/contracts.json";
+import marketplaceABI from "../contracts/MarketplaceABI.json";
+import NFTABI from "../contracts/NFTABI.json";
+import {Container, Typography, Input, TextField, Button} from "@mui/material";
+
+const client = ipfsHttpClient('https://ipfs.infura.io:5001/api/v0')
 
 function Create() {
-  return <Typography variant='h1' component='h1'>Create</Typography>
+  const signer = useSelector(state => state.web3.signer);
+
+  const marketplace = new ethers.Contract(contracts.Marketplace, marketplaceABI, signer);
+  const nft = new ethers.Contract(contracts.NFT, NFTABI, signer);
+
+  const [image, setImage] = useState();
+  const [price, setPrice] = useState('1');
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const navigate = useNavigate();
+
+  const uploadFileHandler = async (e) => {
+    const file = await client.add(e.target.files[0]);
+    setImage(`https://ipfs.infura.io/ipfs/${file.path}`);
+  };
+
+  const handleCreate = async (e) => {
+    e.preventDefault();
+    const result = await client.add(JSON.stringify({ image, price, title, description }));
+    const uri = `https://ipfs.infura.io/ipfs/${result.path}`;
+
+    await (await nft.mint(uri)).wait()
+    const id = await nft.tokenCount()
+    await (await nft.setApprovalForAll(marketplace.address, true)).wait()
+    const listingPrice = ethers.utils.parseEther(price.toString())
+    await (await marketplace.makeItem(nft.address, id, listingPrice)).wait();
+    navigate('/marketplace');
+  };
+
+  return (
+    <>
+      <Typography component='h1' variant='h1'>Create</Typography>
+      <Container maxWidth={false} disableGutters sx={{display: 'flex', flexDirection: 'column', gap: '10px'}}>
+        <Typography component='h5' variant='h5'>Upload file for NFT</Typography>
+
+        <Input type="file" onChange={uploadFileHandler}/>
+        <TextField id="title-input" label="Title" variant="outlined" onChange={e => setTitle(e.target.value)} value={title}/>
+        <TextField id="description-input" label="Description" variant="outlined" onChange={e => setDescription(e.target.value)} value={description}/>
+        <TextField id="price-input" label="Input" variant="outlined" onChange={e => setPrice(e.target.value)} value={price}/>
+        <Button onClick={handleCreate}>Create</Button>
+      </Container>
+    </>
+  )
 }
 
 export default Create;

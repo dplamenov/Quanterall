59d7bb1b92436d83e3e645f804aa493136f253c9
dplamenov <dimitar.plamenov@gmail.com>
2022-06-16 11:27:41 +0300

    Update Marketplace.js

--- frontend/src/pages/Marketplace.js
+++ frontend/src/pages/Marketplace.js
@@ -1,7 +1,53 @@
 import {Typography} from "@mui/material";
+import {useSelector} from "react-redux";
+import {useEffect, useState} from "react";
+import contracts from "../contracts/contracts.json";
+import marketplaceABI from "../contracts/MarketplaceABI.json";
+import NFTABI from "../contracts/NFTABI.json";
+import {ethers} from 'ethers';
+import NFT from "../components/NFT";
 
 function Marketplace() {
-  return <Typography variant='h1' component='h1'>Marketplace</Typography>
+  const signer = useSelector(state => state.web3.signer);
+  const marketplace = new ethers.Contract(contracts.Marketplace, marketplaceABI, signer);
+  const nft = new ethers.Contract(contracts.NFT, NFTABI, signer);
+
+  const [listedItems, setListedItems] = useState([])
+
+  const loadListedItems = async () => {
+    const itemCount = await marketplace.itemCount();
+    const listedItems = []
+
+    for (let index = 1; index <= itemCount; index++) {
+      const i = await marketplace.items(index)
+      const uri = await nft.tokenURI(i.tokenId)
+      const response = await fetch(uri)
+      const metadata = await response.json()
+      const totalPrice = await marketplace.getTotalPrice(i.itemId)
+      const item = {
+        totalPrice,
+        price: i.price,
+        itemId: i.itemId,
+        title: metadata.title,
+        description: metadata.description,
+        image: metadata.image
+      }
+      listedItems.push(item);
+    }
+
+    setListedItems(listedItems)
+  }
+
+  useEffect(() => {
+    loadListedItems()
+  }, [])
+
+  return <>
+    <Typography variant='h1' component='h1'>Marketplace</Typography>
+    {listedItems.map(item => {
+      return <NFT key={item.itemId} item={item}/>
+    })}
+  </>
 }
 
 export default Marketplace;

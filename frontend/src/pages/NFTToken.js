cad132654f5eb9ed38551701f4fed39a36fc885d
dplamenov <dimitar.plamenov@gmail.com>
2022-06-16 14:43:37 +0300

    Update NFTToken.js

--- frontend/src/pages/NFTToken.js
+++ frontend/src/pages/NFTToken.js
@@ -1,18 +1,40 @@
-import {Typography, Container, TextField} from "@mui/material";
+import {Typography, Container, TextField, Button} from "@mui/material";
+import {useState} from "react";
 
 function NFTToken() {
+  const [buyTokens, setBuyTokens] = useState(0);
+  const [buyEth, setBuyEth] = useState(0);
+  const [saleTokens, setSaleTokens] = useState(0);
+  const [saleEth, setSaleEth] = useState(0);
+
+  const buyHandler = () => {
+
+  };
+
+  const sellHandler = () => {
+
+  }
+
   return <>
     <Typography component='h1' variant='h1'>NFT Token</Typography>
     <Container disableGutters maxWidth={false} sx={{display: 'flex'}}>
       <Container disableGutters maxWidth={false}>
         <Typography component='h2' variant='h2'>Buy</Typography>
-        <TextField id="buy-tokens-input" label="Tokens" variant="outlined" />
-        <TextField id="buy-eth-input" label="Eth" variant="outlined"/>
+        <Container disableGutters maxWidth={false} sx={{display: 'flex', gap: '10px'}}>
+          <TextField id="buy-tokens-input" label="Tokens" variant="outlined" value={buyTokens}/>
+          <TextField id="buy-eth-input" label="Eth" variant="outlined" value={buyEth}/>
+        </Container>
+        <p>I will buy COUNT tokens for PRICE eth</p>
+        <Button variant='contained' onClick={buyHandler}>Buy</Button>
       </Container>
       <Container disableGutters maxWidth={false}>
         <Typography component='h2' variant='h2'>Sell</Typography>
-        <TextField id="sell-eth-input" label="Eth" variant="outlined" />
-        <TextField id="sell-tokens-input" label="Tokens" variant="outlined"/>
+        <Container disableGutters maxWidth={false} sx={{display: 'flex', gap: '10px'}}>
+          <TextField id="sell-eth-input" label="Eth" variant="outlined" value={saleTokens}/>
+          <TextField id="sell-tokens-input" label="Tokens" variant="outlined" value={saleEth}/>
+        </Container>
+        <p>I will sell COUNT tokens for PRICE eth</p>
+        <Button variant='contained' onClick={sellHandler}>Sell</Button>
       </Container>
     </Container>
   </>

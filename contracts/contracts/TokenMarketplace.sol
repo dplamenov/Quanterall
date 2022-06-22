989ef446a725d8835bec2ae9f7edb28f625f881c
dplamenov <dimitar.plamenov@gmail.com>
2022-06-22 21:33:27 +0300

    Update TokenMarketplace.sol

--- contracts/contracts/TokenMarketplace.sol
+++ contracts/contracts/TokenMarketplace.sol
@@ -2,23 +2,27 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 contract TokenMarketplace {
+    using SafeMath for uint256;
+
     address payable token;
-    uint256 treasure;
+    uint256 public treasure;
 
     constructor(address _token) {
         token = payable(_token);
     }
 
-    function addLiquidity(uint256 _tokens) public {
+    function addLiquidity(uint256 _tokens) public payable{
         IERC20(token).transferFrom(msg.sender, address(this), _tokens);
+        treasure += _tokens;
     }
 
     function buy() public payable {
-        uint k = address(this).balance * treasure;
-        treasure -= k / msg.value;
-        IERC20(token).transfer(msg.sender, k / msg.value);
+        uint256 k = address(this).balance * treasure;
+        treasure -= msg.value / k;
+        IERC20(token).transfer(msg.sender, msg.value / k);
     }
 
     function sell(uint256 tokenToSale) public payable {
@@ -26,5 +30,13 @@ contract TokenMarketplace {
 
         IERC20(token).transferFrom(msg.sender, address(this), tokenToSale);
         payable(msg.sender).transfer(k / tokenToSale);
+    }
+
+    function getBalance() public view returns(uint256) {
+        return address(this).balance;
+    }
+
+    function getBalanceOfTokens() public view returns(uint256) {
+        return IERC20(token).balanceOf(address(this));
     }
 }

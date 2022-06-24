d3920fb78f0e7d30832450916c086fba3b4a32c0
dplamenov <dimitar.plamenov@gmail.com>
2022-06-24 15:23:41 +0300

    Update Marketplace.js

--- frontend/src/pages/Marketplace.js
+++ frontend/src/pages/Marketplace.js
@@ -54,7 +54,7 @@ function Marketplace() {
         return <NFT key={k} item={item}/>
       })}
     </Container>
-    {listedItems.length === 0 && <Typography variant='h2' component='h2'>No items</Typography>}
+    {listedItems.length === 0 && <Typography variant='h2' component='h2'>No items for sale</Typography>}
   </>
 }
 

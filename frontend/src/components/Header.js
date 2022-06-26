49cac047ec32b75edc44352d3623f2875c4bfd76
dplamenov <dimitar.plamenov@gmail.com>
2022-06-26 16:35:35 +0300

    update header connect button handler

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -2,8 +2,7 @@ import { AppBar, Box, Toolbar, Typography, Button, IconButton } from '@mui/mater
 import { Link } from "react-router-dom";
 import {ethers} from 'ethers';
 import {useDispatch, useSelector} from "react-redux";
-import {connect, loadContracts} from '../stores/web3';
-import contracts from "../contracts/contracts.json";
+import {connect} from '../stores/web3';
 
 function Header() {
   const dispatch = useDispatch();
@@ -13,19 +12,8 @@ function Header() {
     const provider = new ethers.providers.Web3Provider(window.ethereum)
     const accounts = await provider.send("eth_requestAccounts", []);
     dispatch(connect({provider, signer: provider.getSigner(), account: accounts[0]}));
-
-    const contractsMap = {};
-
-    Promise.all(...[Object.entries(contracts).map(async ([name]) => {
-      return [await import('../contracts/' + name + 'ABI.json'), name];
-    })]).then((data) => {
-      data.forEach(contract => {
-        contractsMap[contract[1]] = new ethers.Contract(contracts[contract[1]], contract[0].default, provider.signer);
-      });
-      dispatch(loadContracts({contracts: contractsMap}));
-    });
   };
-  
+
   const ConnectedNavigation = () => {
     return <>
       <Button color="inherit"><Link to="/marketplace">Marketplace</Link></Button>

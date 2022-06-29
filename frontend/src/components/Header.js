2bd8e3344996369ab069241a795315ad2369a778
dplamenov <dimitar.plamenov@gmail.com>
2022-06-29 21:59:07 +0300

    remove icon button in header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -32,14 +32,7 @@ function Header() {
   return <Box component="div">
     <AppBar position="static">
       <Toolbar>
-        <IconButton
-          size="large"
-          edge="start"
-          color="inherit"
-          aria-label="menu"
-          sx={{mr: 2}}
-        >
-        </IconButton>
+
         <Typography variant="h6" component="div" sx={{flexGrow: 1}}>
           NIPFI
         </Typography>

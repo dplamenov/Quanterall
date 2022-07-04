183d292349a43909b118812a3f67345cfb44720b
dplamenov <dimitar.plamenov@gmail.com>
2022-07-04 21:59:54 +0300

    style footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,5 +1,5 @@
 function Footer() {
-  return <div style={{display: 'flex', justifyContent: 'center'}}>
+  return <div style={{position: 'fixed', bottom: '0px', left: '15px'}}>
     <p>Version: 0.5.1/04.07.2022</p>
   </div>
 }
